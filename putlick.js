9eb14606f13fe0ef80ebbfe279b5c16f01f728f8
ちと <chitose.dev@outlook.jp>
2024-11-20 23:17:38 +0900

    Update putlick.js

--- putlick.js
+++ putlick.js
@@ -4,3 +4,5 @@ ytdl.getBasicInfo("http://www.youtube.com/watch?v=aqz-KE-bpKQ").then(info => {
   console.log(info.videoDetails.title);
   return info;
 });
+
+ytdl("http://www.youtube.com/watch?v=aqz-KE-bpKQ").pipe(require("fs").createWriteStream("video.mp4"));
